97f2a458590fdfe859c23b6e72b540ea383fbba2
sino330 <narutokozo330@gmail.com>
2021-11-10 12:22:00 +0900

    2021/11/10

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
-const path =require("path")
+const path = require("path");
 
-module.exports={
-    //始めに読み込むfile
-    entry:"./src/index.tsx",
-    module:{
-        rules:[
-            {
-                test:/\.tsx?$/,
-                use:"ts-loader",
-                exclude:/node_modules/,
-            },
-        ],
-    },
-    resolve:{
-        extensions:[".js",".ts",".tsx"],
-    },
-    //出力するfileの設定
-    output:{
-        path:path.resolve(__dirname,"dist"),
-        filename:"index.js",
-        publicPath:"dist/",
-    }
-}
+module.exports = {
+  //始めに読み込むfile
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  //出力するfileの設定
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    publicPath: "dist/",
+  },
+  devServer: {
+    //fileを変更すると自動的にブラウザに反映させるフラグ
+    hot: true,
+    //起動時にブラウザで開くフラグ
+    open: true,
+  },
+};
