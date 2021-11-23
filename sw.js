85a6cac890bd3523b3bdc0dcaa2ccc26fd3bc2e7
sino330 <narutokozo330@gmail.com>
2021-11-23 10:57:08 +0900

    2021/11/23_vol2

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-const { cache } = require("webpack");
-
-const CacheName="Cache:v1"
+const CacheName = "Cache:v1";
 
 //selfはサービスワーカー自身を指す。
 self.addEventListener("install", (event) => {
@@ -11,20 +9,20 @@ self.addEventListener("activate", (event) => {
   console.log("ServiceWorker activate:", event);
 });
 
-const networkFallingBackToCache=async(request)=>{
-    const cache=await caches.open(CacheName)
-    try{
-        const response=await fetch(request)
-        await cache.put(request,response.clone())
-        return response
-    }catch(err){
-        console.log(err)
-        return cache.match(request)
-    }
-}
+const networkFallingBackToCache = async (request) => {
+  const cache = await caches.open(CacheName);
+  try {
+    const response = await fetch(request);
+    //response内容をcacheに保存
+    await cache.put(request, response.clone());
+    //responseを返却
+    return response;
+  } catch (err) {
+    console.error(err);
+    return cache.match(request);
+  }
+};
 
-self.addEventListener("fetch",(event)=>{
-  //fetchとはnetworkなどを経由してresourceを取得するために使用するAPI
-  console.log("Fetch to:", event, request.url);
-  event.respondWith(event.request);
-})
+self.addEventListener("fetch", (event) => {
+  event.respondWith(networkFallingBackToCache(event.request));
+});
